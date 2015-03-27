afbc6657f2a2e39573d9c6e41f9dc281029c2b7f
ggirelli <gabriele@filopoe.it>
2015-03-27 22:10:26 +0100

    First commit.

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// ANGULAR APP \\
+
+angular.module('advPre', ['ngAnimate'])
+
+	.controller('interfaceController',
+		function($scope) {
+
+			$('[data-toggle="tooltip"]').tooltip()
+
+			/**
+			 * Slider open status
+			 * @type {Boolean}
+			 */
+			$scope.opened = false;
+
+			$scope.imageList = [];
+
+
+			/**
+			 * Whether the slider is open
+			 * @return {Boolean}
+			 */
+			$scope.isOpen = function () { return($scope.opened); }
+
+			/**
+			 * Opens the slider
+			 * @return NULL
+			 */
+			$scope.open = function () {
+				$scope.opened = true;
+			    $scope.slider = window.open('slider.html','slider','modal=yes,alwaysRaised=yes,location=0,menubar=0,resizable=0,fullscreen=1');
+			}
+
+			/**
+			 * Closes the slider
+			 * @return NULL
+			 */
+			$scope.close = function () {
+				$scope.opened = false;
+				$scope.slider.close();
+			}
+
+			/**
+			 * Refreshes the list of images
+			 * @return NULL
+			 */
+			$scope.refresh_image_list = function () {
+				console.log('refresh');
+			}
+
+		}
+	);
